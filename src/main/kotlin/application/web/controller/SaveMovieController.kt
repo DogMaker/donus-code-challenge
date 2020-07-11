861aacf76930e5b74c182e9fa03274d57fb0c4da
Hugo <firefactoryan@gmail.com>
2020-07-11 13:32:45 -0300

    Added new custom object mapper

--- src/main/kotlin/application/web/controller/SaveMovieController.kt
+++ src/main/kotlin/application/web/controller/SaveMovieController.kt
@@ -1,24 +1,27 @@
 package main.application.web.controller
 
-import com.fasterxml.jackson.core.type.TypeReference
+import com.fasterxml.jackson.annotation.JsonInclude.Include
+import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.sun.xml.internal.ws.encoding.soap.DeserializationException
-import org.slf4j.LoggerFactory
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import main.domain.entities.Movie
+import main.domain.entities.MovieResponse
+import main.domain.services.SaveMovieService
+
 
 object SaveMovieController{
 
-     val logger = LoggerFactory.getLogger(this::class.java)
-
-     fun create(json: String):String {
-         //jacksonObjectMapper().readAsTypedValue<Movie>(json)
-         logger.info("Starting route creation process with json $json")
-         return "$json"
+     fun create(json: String): MovieResponse {
+         val json =  objectMapperConfig()?.readValue(json, Movie::class.java)
+         return SaveMovieService().create(json!!)
      }
 }
-inline fun <reified T> ObjectMapper.readAsTypedValue(json: String): T =
-        runCatching { readValue<T>(json, object : TypeReference<T>() {}) }
-                .getOrElse { throw DeserializationException(
-                        json,
-                        T::class.java.name
-                )
-                }
+
+fun objectMapperConfig(): ObjectMapper? {
+    val objectMapper = jacksonObjectMapper()
+    objectMapper.setSerializationInclusion(Include.NON_NULL)
+    objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+
+    return objectMapper
+}
+
