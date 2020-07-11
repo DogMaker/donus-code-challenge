847f4c56d485fb7427489be5dce29d19b6cc14cb
Hugo <firefactoryan@gmail.com>
2020-07-11 13:33:37 -0300

    Blank spaces

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,10 +1,5 @@
 package main
 
-import com.fasterxml.jackson.databind.SerializationFeature
-import io.ktor.application.Application
-import io.ktor.application.install
-import io.ktor.features.ContentNegotiation
-import io.ktor.jackson.jackson
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 import main.application.web.routes.routesModule
@@ -12,17 +7,10 @@ import main.application.web.routes.routesModule
 fun main(args: Array<String>) {
 
     embeddedServer(Netty, port = 8080){
-        jacksonConfig()
         routesModule()
     }.start(wait = true)
 }
-fun Application.jacksonConfig() {
-    install(ContentNegotiation) {
-        jackson {
-            enable(SerializationFeature.INDENT_OUTPUT)
-        }
-    }
-}
+
 
 
 
