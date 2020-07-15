b7622255d83d4a408ca77a2b42553319dc0e205e
Hugo <firefactoryan@gmail.com>
2020-07-14 21:32:56 -0300

    Added second validation

--- src/main/kotlin/application/web/validations/MovieValidations.kt
+++ src/main/kotlin/application/web/validations/MovieValidations.kt
@@ -9,6 +9,7 @@ class MovieValidations {
         if (json.movie.name.isBlank()) ary.add("name")
         if (json.movie.details.cast.contains(" ")
                 ||json.movie.details.cast.contains("")) ary.add("details.cast")
+        if (json.movie.details.cast.size == 0 ) ary.add("details.cast")
         if (json.movie.details.direction.isBlank()) ary.add("details.direction")
 
         return ary
